e85c38cdbdf329de06fde36d08320c51390a5289
Ｖ字龍 <Vdragon.Taiwan@gmail.com>
2015-12-18 05:37:06 +0800

    實作 001 - Getting Started/030 - Repositories/050 - 列出組織所擁有的版本倉庫
    Signed-off-by: Ｖ字龍 <Vdragon.Taiwan@gmail.com>

---
+++ 001 - Getting Started/030 - Repositories/050 - 列出組織所擁有的版本倉庫.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+printf "請輸入您要察看的 GitHub 組織識別名稱（非顯示名稱）："
+read github_organization_identifier
+
+
+curl --include "https://api.github.com/orgs/${github_organization_identifier}/repos"
+
+printf "\n請按 Enter 鍵結束程式。"
+read nothing
+exit 0;
