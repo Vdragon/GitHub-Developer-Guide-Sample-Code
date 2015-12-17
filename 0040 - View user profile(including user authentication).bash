cc322e44af1387d2789bbd60e98ec70653836290
Ｖ字龍 <Vdragon.Taiwan@gmail.com>
2015-12-18 03:54:30 +0800

    新增 View user profile(including user authentication)
    Signed-off-by: Ｖ字龍 <Vdragon.Taiwan@gmail.com>

---
+++ 0040 - View user profile(including user authentication).bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+# https://developer.github.com/guides/getting-started/#basic
+printf "請輸入您的使用者識別名稱（非顯示名稱）："
+read github_user_name
+printf "請輸入要查詢的 GitHub 使用者識別名稱（非顯示名稱）："
+read github_query_user_name
+curl --include --user ${github_user_name} https://api.github.com/users/${github_query_user_name}
+
+exit 0;
