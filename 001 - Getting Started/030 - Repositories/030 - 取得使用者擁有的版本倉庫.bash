32e27e855774c9032c9b30eaa6b621308a216668
Ｖ字龍 <Vdragon.Taiwan@gmail.com>
2015-12-18 05:32:40 +0800

    實作 001 - Getting Started/030 - Repositories/030 - 取得使用者擁有的版本倉庫.bash
    Signed-off-by: Ｖ字龍 <Vdragon.Taiwan@gmail.com>

---
+++ 001 - Getting Started/030 - Repositories/030 - 取得使用者擁有的版本倉庫.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+printf "請輸入您的 GitHub API 身份象徵物(token)："
+read github_api_token
+
+curl --include --header "Authorization: token ${github_api_token}" https://api.github.com/user/repos
+
+printf "\n請按 Enter 鍵結束程式。"
+read nothing
+exit 0;
