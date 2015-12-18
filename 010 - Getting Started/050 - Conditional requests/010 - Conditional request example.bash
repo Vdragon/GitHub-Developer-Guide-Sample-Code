3c77b6798a5bed1eeb2d5c34657cb7d4cb9a0499
Ｖ字龍 <Vdragon.Taiwan@gmail.com>
2015-12-18 11:12:40 +0800

    Refactor - 序號加上一位末位，預留未來新增的編號空間
    Signed-off-by: Ｖ字龍 <Vdragon.Taiwan@gmail.com>

---
+++ 010 - Getting Started/050 - Conditional requests/010 - Conditional request example.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+printf "請輸入要查詢的 GitHub 使用者識別名稱（非顯示名稱）："
+read github_user_name
+
+# -i, --include: (HTTP) Include the HTTP-header in the output. The HTTP-header includes things like server-name, date of the document, HTTP-version and more...
+curl --include https://api.github.com/users/${github_user_name}
+
+printf "\n請按 Enter 鍵結束程式。"
+read nothing
+exit 0
